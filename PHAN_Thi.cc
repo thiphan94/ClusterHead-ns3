3cc7db79c718e4de8abad35f244c98002c3b86ba
Thi Phan <thiphan94@gmail.com>
2021-03-27 23:29:42 +0100

    nodes

---
+++ PHAN_Thi.cc
@@ -0,0 +1,80 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+
+#include "ns3/netanim-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("DataCenter2");
+
+int main (int argc, char *argv[])
+{
+	bool verbose = true;
+	int i;
+	int j = 1;
+	int cluster = 0;
+	if (verbose)
+	{
+		LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+		LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+	}
+
+
+	//Create 50 nodes
+	NodeContainer nodes;
+	nodes.Create(50);
+
+	// Define the Point-To-Point Links and their Paramters
+	PointToPointHelper pointToPoint;
+	pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+	pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+
+	NetDeviceContainer devices;
+	for (int k = 1; k < 6; ++k){
+		i = j;
+		for (i = j; i < j+9; ++i)
+		{
+				devices = pointToPoint.Install(nodes.Get(i), nodes.Get(cluster));
+		}
+		j += 10;
+		cluster +=10;
+
+	}
+
+	InternetStackHelper stack;
+	stack.Install (nodes);
+
+	Ipv4AddressHelper address;
+	address.SetBase ("10.1.1.0", "255.255.255.0");
+
+	Ipv4InterfaceContainer interfaces = address.Assign (devices);
+
+	UdpEchoServerHelper echoServer (9);
+
+	ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+	serverApps.Start (Seconds (1.0));
+	serverApps.Stop (Seconds (10.0));
+
+	UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+	echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+	echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+	echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+	ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+	clientApps.Start (Seconds (2.0));
+	clientApps.Stop (Seconds (10.0));
+
+
+
+  AnimationInterface anim("Thi.xml");
+
+	Simulator::Run ();
+	Simulator::Destroy ();
+	return 0;
+}
